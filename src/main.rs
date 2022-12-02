5f6a05dddc06548248c317fa3ec222a0f7bec396
Daniil Ubica <acveloff@gmail.com>
2022-12-02 20:13:29 +0300

    file sparse_matrix

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,11 @@
-use queues::*;
 use rand::Rng;
-use std::cmp::Ordering;
 use std::fs::File;
 use std::io::prelude::*;
-use std::time::Instant;
+use queues::*;
 
-struct Element{
-  value: i16,
-  row: usize,
-  column: usize,
-}
+mod sparse_matrix;
 
-struct Matrix{
-    columns: usize,
-    rows: usize,
-    matrix: Vec<Vec<i16>>,
-    elements: Vec<Element>,
-}
-
-impl Element {
-  fn new(row: usize, column: usize, value: i16) -> Element {
-    Element {
-      row: row,
-      column: column,
-      value: value,
-    }
-  }
-}
-
-impl Matrix {
-    fn new(r: usize, c: usize) -> Matrix {
-      Matrix{
-        rows: r,
-        columns: c,
-        matrix: vec![vec![0; c]; r],
-        elements: vec![],
-      }
-    }
-
-    fn print(&self){
-    for i in 0..self.rows{
-      for j in 0..self.columns{
-        print!("{} ", self.matrix[i][j]);
-      }
-      println!();
-    }
-  }
-
-  fn set_matrix(&mut self) {
-    self.matrix = vec![vec![0; self.columns]; self.rows];
-
-    if self.elements.len() > 0{
-      for i in 0..self.elements.len() {
-        if self.elements[i].row < self.rows && self.elements[i].column < self.columns {
-          self.matrix[self.elements[i].row][self.elements[i].column] = self.elements[i].value;
-        }
-      }
-    }
-  }
-
-  fn read_elements(&mut self, arr: Vec<Element>) {
-    while self.elements.len() > 0 {
-      self.elements.pop().unwrap();
-    }
-
-    for i in 0..arr.len() {
-      let element = Element{row: arr[i].row, column: arr[i].column, value: arr[i].value};
-      self.elements.push(element);
-    }
-  }
-
-  fn swap_elements(&mut self, number: i16) {
-    let mut bigger: Queue<&Element> = Queue::new();
-    let mut less: Queue<&Element> = Queue::new();
-    let mut arr: Vec<Element> = vec![];
-
-    for i in 0..self.elements.len() {
-      match &self.elements[i].value.cmp(&number) {
-        Ordering::Less => less.add(&self.elements[i]),
-        Ordering::Greater => bigger.add(&self.elements[i]),
-        Ordering::Equal => less.add(&self.elements[i]),
-      };
-    }
-
-    for i in 0..self.rows {
-      for j in 0..self.columns {
-        if &self.matrix[i][j] != &0 {
-          if bigger.size() > 0 && less.size() >= 1 {
-            let element = Element{row: i, column:j, value: bigger.peek().unwrap().value};
-            arr.push(element);
-            bigger.remove().ok().expect("Error in removing");
-          }
-          else if less.size() > 0 {
-            let element = Element{row: i, column:j, value: less.peek().unwrap().value};
-            arr.push(element);
-            less.remove().ok().expect("Error in removing");
-          }
-        }
-      }
-    }
-    self.read_elements(arr);
-    self.set_matrix();
-  }
-}
+use sparse_matrix::sparse_matrix::*;
 
 fn parse_file(matrix: &mut Matrix, file_name: String) -> Vec<Element> {
 
@@ -160,17 +63,12 @@ fn generate_file (size: i32, file_name: String) {
 
 fn main() {
   
-  generate_file(100, "file.txt".to_string());
+  generate_file(3, "file.txt".to_string());
 
   let mut m: Matrix = Matrix::new(3, 3);
   let q = parse_file(&mut m, "file.txt".to_string());
 
   m.read_elements(q);
-  m.set_matrix();
-  let start = Instant::now();
   m.swap_elements(30);
-  let time = start.elapsed();
-  println!("Time elapsed in expensive_function() is: {:?}", time);
-  //m.print();  
-  println!("{}", std::mem::size_of::<Element>());
+  m.print();  
 }
---
+++ src/sparse_matrix.rs
@@ -0,0 +1,106 @@
+pub mod sparse_matrix {
+
+    use std::cmp::Ordering;
+    use queues::*;
+    
+      pub struct Element{
+        value: i16,
+        row: usize,
+        column: usize,
+      }
+    
+      pub struct Matrix{
+          pub columns: usize,
+          pub rows: usize,
+          matrix: Vec<Vec<i16>>,
+          elements: Vec<Element>,
+      }
+    
+      impl Element {
+        pub fn new(row: usize, column: usize, value: i16) -> Element {
+          Element {
+            row: row,
+            column: column,
+            value: value,
+          }
+        }
+      }
+    
+      impl Matrix {
+          pub fn new(r: usize, c: usize) -> Matrix {
+            Matrix{
+              rows: r,
+              columns: c,
+              matrix: vec![vec![0; c]; r],
+              elements: vec![],
+            }
+          }
+    
+          pub fn print(&self){
+          for i in 0..self.rows{
+            for j in 0..self.columns{
+              print!("{} ", self.matrix[i][j]);
+            }
+            println!();
+          }
+        }
+    
+        fn set_matrix(&mut self) {
+          self.matrix = vec![vec![0; self.columns]; self.rows];
+    
+          if self.elements.len() > 0{
+            for i in 0..self.elements.len() {
+              if self.elements[i].row < self.rows && self.elements[i].column < self.columns {
+                self.matrix[self.elements[i].row][self.elements[i].column] = self.elements[i].value;
+              }
+            }
+          }
+        }
+    
+        pub fn read_elements(&mut self, arr: Vec<Element>) {
+          while self.elements.len() > 0 {
+            self.elements.pop().unwrap();
+          }
+    
+          for i in 0..arr.len() {
+            let element = Element{row: arr[i].row, column: arr[i].column, value: arr[i].value};
+            self.elements.push(element);
+          }
+          self.set_matrix();
+        }
+    
+        pub fn swap_elements(&mut self, number: i16) {
+          let mut bigger: Queue<&Element> = Queue::new();
+          let mut less: Queue<&Element> = Queue::new();
+          let mut arr: Vec<Element> = vec![];
+    
+          for i in 0..self.elements.len() {
+            match &self.elements[i].value.cmp(&number) {
+              Ordering::Less => less.add(&self.elements[i]),
+              Ordering::Greater => bigger.add(&self.elements[i]),
+              Ordering::Equal => less.add(&self.elements[i]),
+            };
+          }
+    
+          for i in 0..self.rows {
+            for j in 0..self.columns {
+              if &self.matrix[i][j] != &0 {
+                if bigger.size() > 0 && less.size() >= 1 {
+                  let element = Element{row: i, column:j, value: bigger.peek().unwrap().value};
+                  arr.push(element);
+                  bigger.remove().ok().expect("Error in removing");
+                }
+                else if less.size() > 0 {
+                  let element = Element{row: i, column:j, value: less.peek().unwrap().value};
+                  arr.push(element);
+                  less.remove().ok().expect("Error in removing");
+                }
+              }
+            }
+          }
+          self.read_elements(arr);
+          self.set_matrix();
+        }
+      }
+    
+    }
